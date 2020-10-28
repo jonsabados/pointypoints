62afbf331a25d4d4cfcecc08bdf16740dd634e6e
Trace <teohrt18@gmail.com>
2020-10-28 14:30:00 -0700

    Typo fix

--- frontend/src/home/Home.vue
+++ frontend/src/home/Home.vue
@@ -2,7 +2,7 @@
   <main class="container-fluid" role="main">
     <h1>PointyPoints Task Pointing</h1>
     <p>
-      Welcome to PointPoints task pointing, a server-less task pointing application/fun way to play with an application
+      Welcome to PointyPoints task pointing, a server-less task pointing application/fun way to play with an application
       making use of WebSockets on the cheap. All server side functionality for this application is implemented via
       lambdas exposed through API Gateway. Dynamo is used for the data store. The front end is a Vue.js application
       stuffed into an S3 bucket and exposed to the world with CloudFront.
